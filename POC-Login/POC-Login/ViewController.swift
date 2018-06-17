f84ae26a6e64483aaded8c0c6b53d90d52ddab5b
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2018-06-17 02:35:41 -0300

    view end editing

--- POC-Login/POC-Login/ViewController.swift
+++ POC-Login/POC-Login/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK: Tocuhes
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //resign view or any subviews as first responders when touched
+        self.view.endEditing(false)
+    }
 }
-
