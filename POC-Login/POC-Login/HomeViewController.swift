59d68d637291c4306d01efaa8a3011aad66347c6
Felipe Marino <felipemarino91@gmail.com>
2018-06-19 12:00:40 -0300

    home sceen + logout

---
+++ POC-Login/POC-Login/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  POC-Login
+//
+//  Created by Felipe Lefevre Marino on 19/06/18.
+//  Copyright © 2018 Felipe Marino. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+
+    @IBAction func logout(sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
